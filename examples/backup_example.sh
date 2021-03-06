32d9b73a132abc1be8e314a5d9a4a93f0f91fd4d
Rune Hattmann <runehattmann@gmail.com>
2021-03-06 00:46:47 +0000

    updated example files month to days

--- examples/backup_example.sh
+++ examples/backup_example.sh
@@ -4,9 +4,9 @@
 source="${remote}:files/photos" # What ever path to source
 dest="${encrypt}:some_backup_name" # Path to destination
 date_for_backup="01" #two digit number ex. 01 for the first in each month to run the script
-del_after="3" # Will will delete everything older than x month in bckp
+del_after="90" # Will will delete everything older than x days in bckp
 keep_mnt="01,07" # Keep these backup months in the old_dir ex. 01 or 01,04,07,10(comma seperated)
-del_all_after="18" # Will delete everything older than x month in old_dir
+del_all_after="365" # Will delete everything older than x days in old_dir
 job_name="$(basename $0)" # This is the this files name, but you can change it to what ever you decide
 options="--dry-run" # remove --dry-run hook when you have tested the file
 
--- examples/sync_example.sh
+++ examples/sync_example.sh
@@ -4,7 +4,7 @@
 source="${remote}:files/photos" # What ever path to source
 dest="${encrypt}:some_backup_name" # Path to destination
 job_name="$(basename $0)" # This is the this files name, but you can change it to what ever you decide
-retention="300" # How many days back in time, to collect old files
+retention="30" # How many days back in time, to collect old files
 options="--dry-run" # remove --dry-run hook when you have tested the file
 
 full/path/to/sync.sh "$source" "$dest" "$job_name" "$retention" "$options"
