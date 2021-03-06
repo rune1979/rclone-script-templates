89fc07c97d03e61d4c5fbc45911f5b40bcb52dd7
Rune Hattmann <runehattmann@gmail.com>
2021-03-06 00:10:25 +0000

    delete function updated

--- sync.sh
+++ sync.sh
@@ -5,11 +5,8 @@
 ################################# parameters #################################
 source="$1"    #the directory to back up (without a trailing slash)
 dest="$2"      #the directory to back up to (without a trailing slash or "last_snapshot") destination=$dest/last_snapshot
-
 job_name="$3"          #job_name="$(basename $0)"
-
 retention="$4" # How many days do you want to retain old files for
-
 options="$5"           #rclone options like "--filter-from=filter_patterns --checksum --log-level="INFO" --dry-run"
                        #do not put these in options: --backup-dir, --suffix, --log-file
 
@@ -18,7 +15,6 @@ email="your_email@some_mail.com" # the admin email
 # $new is the directory name of the current snapshot
 # $timestamp is time that old file was moved out of new (not time that file was copied from source)
 new="last_snapshot"
-#timestamp="$(date +%F_%T)"
 timestamp="$(date +%F_%H%M%S)"  #time w/o colons if thumb drive is FAT format, which does not allow colons in file name
 
 # set log_file path
@@ -105,7 +101,7 @@ if pidof -o $PPID -x "$job_name"; then
 fi
 
 ############################### move_old_files_to #############################
-backup_dir="--backup-dir=$dest/archive/$(date +%Y)/$timestamp"
+backup_dir="--backup-dir=$dest/sync_archive/$timestamp"
 
 ################################### back up ##################################
 cmd="rclone sync $source $dest/$new $backup_dir $log_option $options"
@@ -122,20 +118,30 @@ eval $cmd
 exit_code=$?
 conf_logging "$exit_code"
 
-################################### clean up old ##################################
-cmd_delete="rclone delete --min-age ${retention}d $dest/archive/ $log_option -i $options" # you might want to dry-run this too.Remove files
-cmd_rmdir="rclone rmdirs $dest/archive/ $log_option -v $options" # Remove dirs
-echo "Removing old synced files $timestamp $job_name"
-echo "$cmd_delete"
-eval $cmd_delete
-exit_code=$?
-if ! [ $exit_code == 3 ]; then # We don't want any alerts on 3 (no directories found)
-        conf_logging "$exit_code"
-fi
-eval $cmd_rmdir
-exit_code=$?
-if ! [ $exit_code == 3 ]; then # We don't want any alerts on 3 (no directories found)
-        conf_logging "$exit_code"
-fi
+################################### clean up old function ##################################
+delete_dir() {
+    dir="$1"
+    cmd_delete="rclone purge $dest/sync_archive/$dir $log_option $options" # you might want to dry-run this.
+    echo "Removing old synced files $timestamp $job_name"
+    echo "$cmd_delete"
+    eval $cmd_delete
+    exit_code=$?
+    if ! [ $exit_code == 3 ]; then # We don't want any alerts on 3 (no directories found)
+            conf_logging "$exit_code"
+    fi
+}
+
+CMD_LSD="rclone lsd --max-depth 1 $dest/sync_archive/"
+mapfile -t dir_array < <(eval $CMD_LSD)
+DATE=$(date -d "$now - $days days" +'%Y-%m-%d')
+for i in "${!dir_array[@]}"; do
+        dir_path="${dir_array[i]}"
+        dir_date=$(echo ${dir_path##* })
+        dir_date2=$(echo ${dir_date%_*})        
+        conv_date=$(date -d "$dir_date2" +'%Y-%m-%d')
+        if [[ $conv_date > $DATE ]];then
+                delete_dir "$dir_date"
+        fi
+done
 
 exit 0
